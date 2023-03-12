9403a10c51f0027df6c970aa59cd5514ca5d0d23
YuraLen <92933581+YuraLen@users.noreply.github.com>
2023-03-12 19:55:37 +0200

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,122 @@
+import javax.rmi.ssl.SslRMIClientSocketFactory;
+import java.util.Calendar;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("-- 1 task -- ");
+
+        int n = 50;
+        if (n >= 50 && n <= 100){
+            System.out.println("Успішно!  Число " + n + " входить в проміжок (50 - 100)!");
+        } else {
+            System.out.println("Помилка!  Число " + n + " не входить в проміжок (50 - 100)!");
+        }
+
+        System.out.println("-- 2 task--");
+
+        n = 549;
+
+        int max = 0;
+
+        while (n > 0) {
+            int digit = n % 10; // остання цифра числа
+
+            if (digit > max) { // якщо в нас цифра більша за записане max, то перезаписується значення max
+                max = digit;
+            }
+            n /= 10; // відкидаємо останню цифру для переходу до наступної
+        }
+
+        System.out.println("Найбільша цифра числа - " + max);
+
+        System.out.println("-- 3 task --");
+
+        int currentFloor = 9;
+        String direction = "До низу";
+
+        if(currentFloor < 1 || currentFloor > 9){
+            System.out.println("Невірне введення поверху!");
+            } else {
+                while (currentFloor <= 9 && currentFloor >= 1) {
+                    if (direction == "До низу") {
+                        if (currentFloor == 2) {
+                            currentFloor = 1;
+                            System.out.println("Ліфт доставлений на 1-й поверх, замість 2-го");
+                        } else {
+                            System.out.println("Ви спустились на " + currentFloor + " поверх");
+                            currentFloor--;
+                        }
+                    } else if (direction == "До гори") {
+                        if (currentFloor == 1) {
+                        System.out.println("Ліфт піднявся з 1-го поверха");
+                        currentFloor = 3;
+                        System.out.println("Ліфт доставлений на 3-й поверх, замість 2-го");
+                        } else {
+                        System.out.println("Ви піднялись на " + currentFloor + " поверх");
+                        currentFloor++;
+                    }
+                }
+            }
+        }
+
+        System.out.println("-- 4 task --");
+
+        String choice = "no";
+
+        switch (choice){
+            case "Так":
+            case "OK":
+            case "Yes":
+            case "Y":
+            case "+":
+            case "Ok":
+            case "ok":
+                System.out.println("Я погоджуюсь!");
+                break;
+            case "Ні":
+            case "NO":
+            case "N":
+            case "-":
+            case "No":
+            case "no":
+                System.out.println("Я відмовляюсь!");
+                break;
+        }
+
+        System.out.println("-- 5 task --");
+
+        int a = 731, b = 51, c = 122;
+        int  num;
+
+        if (a > b) {
+            num = a;
+            a = b;
+            b = num;
+        }
+
+        if (b > c) {
+            num = b;
+            b = c;
+            c = num;
+        }
+
+        if (a > b) {
+            num = a;
+            a = b;
+            c = num;
+        }
+
+        System.out.printf("Числа в порядку зростання %d, %d, %d.\n", a, b, c);
+
+        System.out.println("-- 6 завдання --");
+
+        n = 1241;
+        int hoursLeft = (n / 3600);
+        if(n <= 3600){
+            System.out.println("До кінця робочого дня залишилось менше 1 години.");
+        } else {
+            System.out.println("Залишилось " + hoursLeft + " годин до кінця робочого дня.");
+        }
     }
 }
